4cd41d562353e813757ef3cd804e5d22fb01fb95
Dhiru Kholia <dhiru.kholia@gmail.com>
2023-04-08 06:50:53 +0530

    Batch Update 2 - September 2023
    Changes:
    
    - Update to OpenCore 0.9.5
    
    - Switch around the emulated video device (required for Ventura)
    
    - Change default resolution to 1920x1080
    
    - Include ventura in shortname CLI help
    
    - Add notes on 'bridged networking'
    
    - Better networking documentation (thomaspaulb)
    
    - Support for offline installations (kwadwokyeremeh)
    
    - Initial macOS Sonoma support (via 'luchina-gabriel') for Intel CPUs

--- OpenCore-Boot.sh
+++ OpenCore-Boot.sh
@@ -13,12 +13,12 @@
 # NOTE: Tweak the "MY_OPTIONS" line in case you are having booting problems!
 ############################################################################
 
-MY_OPTIONS="+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
+MY_OPTIONS="+ssse3,+sse4.2,+popcnt,+avx,+avx2,+aes,+xsave,+xsaveopt,check"
 
 # This script works for Big Sur, Catalina, Mojave, and High Sierra. Tested with
 # macOS 10.15.6, macOS 10.14.6, and macOS 10.13.6.
 
-ALLOCATED_RAM="4096" # MiB
+ALLOCATED_RAM="8192" # MiB
 CPU_SOCKETS="1"
 CPU_CORES="2"
 CPU_THREADS="4"
@@ -28,34 +28,22 @@ OVMF_DIR="."
 
 # shellcheck disable=SC2054
 args=(
-  -enable-kvm -m "$ALLOCATED_RAM" -cpu Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
+  -enable-kvm -m "$ALLOCATED_RAM" -cpu host,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,"$MY_OPTIONS"
   -machine q35
   -usb -device usb-kbd -device usb-tablet
   -smp "$CPU_THREADS",cores="$CPU_CORES",sockets="$CPU_SOCKETS"
-  -device usb-ehci,id=ehci
-  # -device usb-kbd,bus=ehci.0
-  # -device usb-mouse,bus=ehci.0
-  -device nec-usb-xhci,id=xhci
-  -global nec-usb-xhci.msi=off
-  # -device usb-host,vendorid=0x8086,productid=0x0808  # 2 USD USB Sound Card
-  # -device usb-host,vendorid=0x1b3f,productid=0x2008  # Another 2 USD USB Sound Card
   -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
   -drive if=pflash,format=raw,readonly=on,file="$REPO_PATH/$OVMF_DIR/OVMF_CODE.fd"
-  -drive if=pflash,format=raw,file="$REPO_PATH/$OVMF_DIR/OVMF_VARS-1024x768.fd"
+  -drive if=pflash,format=raw,file="$REPO_PATH/$OVMF_DIR/OVMF_VARS-1920x1080.fd"
   -smbios type=2
-  -device ich9-intel-hda -device hda-duplex
-  -device ich9-ahci,id=sata
-  -drive id=OpenCoreBoot,if=none,snapshot=on,format=qcow2,file="$REPO_PATH/OpenCore/OpenCore.qcow2"
-  -device ide-hd,bus=sata.2,drive=OpenCoreBoot
-  -device ide-hd,bus=sata.3,drive=InstallMedia
-  -drive id=InstallMedia,if=none,file="$REPO_PATH/BaseSystem.img",format=raw
-  -drive id=MacHDD,if=none,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
-  -device ide-hd,bus=sata.4,drive=MacHDD
+  -drive id=OpenCoreBoot,if=virtio,format=qcow2,file="OpenCore/OpenCore.qcow2"
+  -drive id=InstallMedia,if=virtio,file="$REPO_PATH/BaseSystem.img",format=raw
+  -drive id=MacHDD,if=virtio,file="$REPO_PATH/mac_hdd_ng.img",format=qcow2
   # -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
-  -netdev user,id=net0 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-pci,netdev=net0,id=net0,mac=52:54:00:c9:18:27
   # -netdev user,id=net0 -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27  # Note: Use this line for High Sierra
   -monitor stdio
-  -device VGA,vgamem_mb=128
+  -device VGA,vgamem_mb=256
 )
 
 qemu-system-x86_64 "${args[@]}"
